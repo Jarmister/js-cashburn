e8e40f224fd1d862c9e24421f7dd56603284c39d
KKrzelowska <61658316+KKrzelowska@users.noreply.github.com>
2020-11-19 15:02:55 +0100

    iss2::Update Nameform

--- src/components/NameForm.tsx
+++ src/components/NameForm.tsx
@@ -1,46 +1,10 @@
 import React, {ChangeEventHandler, EventHandler, FormEventHandler, MouseEventHandler, SyntheticEvent} from "react";
 import ReactDOM from "react-dom";
-import styled, {keyframes} from "styled-components";
+import {Button, Input, Label} from "../styled/styledNameform";
 
 type Props = { callback: (arg: number) => void };
 type State = { value: number };
 
-
-const Button = styled.button`
-display: inline-block;
-background: papayawhip;
-padding: 0.5em;
-margin: 0.5em;
-border: none;
-color: palevioletred;
-  font-weight: bold;`
-
-const Label = styled.label`
-color: palevioletred;
-font-weight: bold;`
-
-const Input = styled.input`
-color: palevioletred;
-font-weight: bold;
-padding: 0.5em;
-margin: 0.5em;
-background: papayawhip;
-`
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-
-  to {
-    transform: rotate(360deg);
-  }
-`;
-const Rotate = styled.div`
-  display: inline-block;
-  animation: ${rotate} 2s linear infinite;
-  padding: 2rem 1rem;
-  font-size: 1.2rem;
-`;
 
 class NameForm extends React.Component<Props, State> {
     handleChange: ChangeEventHandler<HTMLInputElement>;
@@ -67,7 +31,6 @@ class NameForm extends React.Component<Props, State> {
                     value={this.state.value}
                     onChange={this.handleChange}/>
                 <Button onClick={this.handleSubmit}>Zatwierdź</Button>
-                <Rotate>Hedgehog</Rotate>
             </div>
 
         );
